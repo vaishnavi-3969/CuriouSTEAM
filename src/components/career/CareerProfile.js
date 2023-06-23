5d18f93ddb3750e0ded442a5454db68c124c3369
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-24 01:38:58 +0530

    intialization

---
+++ src/components/career/CareerProfile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CareerProfile() {
+  return (
+    <div>CareerProfile</div>
+  )
+}
---
+++ src/components/education/SkillDevpt.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SkillDevpt() {
+  return (
+    <div>SkillDevpt</div>
+  )
+}
