e01d54022b3b2724606044df973604fce6fd38c0
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-05-01 14:31:05 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import EducationalPathways from "./components/educational-pathways/EducationalPa
 import IndustryTrends from "./components/industry-trends/IndustryTrends";
 import EventsAndConferences from "./components/events/EventsAndConferences";
 import ResourcesAndTools from "./components/tools/ResourcesAndTools";
-// import Footer from './components/layout/Footer';
 
 function App() {
   const [sidebarVisible, setSidebarVisible] = useState(true);
@@ -43,7 +42,6 @@ function App() {
               <Route path="/events" element={<EventsAndConferences/>} exact/>
               <Route path="/resources" element={<ResourcesAndTools/>} exact/>
             </Routes>
-            {/* <Footer /> */}
           </div>
         </SidebarContext.Provider>
       </BrowserRouter>
